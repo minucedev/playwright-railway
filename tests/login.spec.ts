87477761be2142eefed7e9f79756bd4d89d66c7f
minucedev <ngnmihpuc.1502@gmail.com>
2025-12-22 10:58:45 +0700

    remove redundant code

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "../src/fixtures/pom.fixtures";
+import { test } from "../src/fixtures/pom.fixtures";
 import { validUser, invalidUserBlankUsername } from "../src/types/users";
 
 test.describe("Login Functionality", () => {
